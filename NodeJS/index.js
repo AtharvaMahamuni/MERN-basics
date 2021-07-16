f71c06b25614ed85012fcf387ff211b003e0e139
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2021-07-16 23:05:17 +0530

    Express basics

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -3,3 +3,10 @@ const atharva = require('./second');
 
 console.log("Hello World", atharva);
 
+console.log("Hello World", atharva.name);
+
+function sayName(name) {
+    console.log("Hello", name);
+}
+
+sayName(atharva.name);
